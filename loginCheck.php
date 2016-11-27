138ab4859b465e463f140ae682e20d5798a4f603
Michal <mdrobena@rgu.ac.uk>
2016-11-27 18:31:58 +0000

    h

--- loginCheck.php
+++ loginCheck.php
@@ -9,33 +9,25 @@ session_start();
 
 //including connection to my db
 include ("dbconnect.php");
-if ($db) {
-    echo 'good db';
-}
-else {
-    echo mysqli_connect_errno() . PHP_EOL;
-    echo mysqli_connect_error() . PHP_EOL;
-}
-
-
 
 //POST method to get user's ID and password from index.html
-$user = $_POST['userID'];
-$password = $_POST['password'];
+$user = $_POST["userID"];
+$password = $_POST["password"];
 
 //a query that returns the user's role(admin or user)
 $sql = "SELECT user_role FROM people WHERE user_name = '$user' AND user_password = '$password'";
 
 //mysqli_query takes two parameters $db(db connection) and $sql variable
 $result = mysqli_query($db, $sql);
+$role = $result->fetch_field_direct(0);
 
-if($result == "user"){
+if($role == "user"){
     /*session_start();
     $_SESSION['access_level'] = "standarduser";
     setcookie('access_level', 'standarduser');*/
     header("Location:userView.php");
 }
-elseif($result == "admin"){
+elseif($role == "admin"){
     header("Location:adminView.php");
 }
 else{
