8f7727639cff31b45305cfcf7eb71b332a89c7b9
Michal <mdrobena@rgu.ac.uk>
2016-11-27 17:20:25 +0000

    h

--- loginCheck.php
+++ loginCheck.php
@@ -13,7 +13,7 @@ $user = $_POST["userID"];
 $password = $_POST["password"];
 
 //a query that returns the user's role(admin or user)
-$sql = "SELECT user_role FROM people WHERE user_name = ".$user." AND user_password = ".$password;
+$sql = "SELECT user_role FROM people WHERE user_name = '$user' AND user_password = '$password'";
 
 //mysqli_query takes two parameters $db(db connection) and $sql variable
 $result = mysqli_query($db, $sql);
