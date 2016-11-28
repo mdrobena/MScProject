a9d9eca19030d848251cbf299ed5abd4838ed8be
Michal <mdrobena@rgu.ac.uk>
2016-11-28 12:17:33 +0000

    new

--- adminView.php
+++ adminView.php
@@ -1,14 +1,76 @@
 <!DOCTYPE html>
-<html>
-<head></head>
-    <body>
-        <P>Hello admin!</P>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Pro</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="css/adminView.css">
+    <link rel="icon" href="image/favicon.ico">
+    <script src="jquery-3.1.1.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        } );
+    </script>
+</head>
+<body>
+    <main>
+        <div id="table">
+            <?php
+                include ("dbconnect.php");
+                session_start();
 
-    <?php
-        echo "haha";
-    ?>;
+                $sql = "SELECT * FROM people";
+                $result = mysqli_query($db, $sql);
 
-    </body>
+                $count = mysqli_num_rows($result);
+                $row = $result -> fetch_assoc();
+                $role = $row['user_role'];
+
+                if ($count > 0){?>
+                    <table id="example" class="display" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>User ID</th>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>User name</th>
+                                <th>User password</th>
+                                <th>Company</th>
+                                <th>User role</th>
+                                <th>Start date</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>User ID</th>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>User name</th>
+                                <th>User password</th>
+                                <th>Company</th>
+                                <th>User role</th>
+                                <th>Start date</th>
+                            </tr>
+                        </tfoot>
+                        <tbody><?php while ($row = mysqli_fetch_assoc($result)){?>
+                        <tr>
+                            <td><?php echo $row['user_id']?></td>
+                            <td><?php echo $row['first_name']?></td>
+                            <td><?php echo $row['last_name']?></td>
+                            <td><?php echo $row['user_name']?></td>
+                            <td><?php echo $row['user_password']?></td>
+                            <td><?php echo $row['company']?></td>
+                            <td><?php echo $row['user_role']?></td>
+                            <td><?php echo $row['start_date']?></td>
+                        </tr>
+                        <?php } ?>
+                        </tbody>
+            <?php}
+            ?>
+        </div>
+    </main>
+</body>
 
 
 </html>
