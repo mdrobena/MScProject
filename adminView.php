7423beeaa4f5eac1ac0462d6b4a9cc08aa50936e
Michal <mdrobena@rgu.ac.uk>
2016-11-29 13:30:41 +0000

    new

--- adminView.php
+++ adminView.php
@@ -11,6 +11,8 @@
 
     <title>Admin</title>
 
+    <script type="text/javascript" src="jquery-3.1.1.min.js"></script>
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -19,8 +21,6 @@
 
     <!-- Custom styles for this template -->
     <link href="css/sticky-footer-navbar.css" rel="stylesheet">
-
-    <script type="text/javascript" src="jquery-3.1.1.min.js"></script>
 
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
 
