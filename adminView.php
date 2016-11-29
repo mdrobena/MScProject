a0378a09b02bc38e377a5bc4a757c8d31fd3dcd5
Michal <mdrobena@rgu.ac.uk>
2016-11-29 14:42:40 +0000

    new

--- adminView.php
+++ adminView.php
@@ -38,10 +38,18 @@
 <body>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">Admin portal</a>
+            </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li></li><p>Admin portal</p></li>
-                    <li><a class="b" href="adminView.php">Home</a></li>
+                    <li><a href="adminView.php">Home</a></li>
                     <li><a href="#">Add user/admin</a></li>
                     <li><a href="#">Delete user/admin</a></li>
                 </ul>
