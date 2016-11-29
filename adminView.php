8269ee996b725a36ce9af3244512dd17347725c9
Michal <mdrobena@rgu.ac.uk>
2016-11-29 13:54:50 +0000

    new

--- adminView.php
+++ adminView.php
@@ -45,11 +45,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Project name</a>
+                <a class="navbar-brand" href="#">Admin portal</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                     <li class="dropdown">
