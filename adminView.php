4d70da51ae634dacc873192db4c2fdaa3a1b1bcb
Michal <mdrobena@rgu.ac.uk>
2016-11-29 14:10:16 +0000

    new

--- adminView.php
+++ adminView.php
@@ -41,11 +41,11 @@
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
+                    <span class="icon-bar">ggfdgfdg</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Admin portal</a>
+                <a class="navbar-brand">Admin portal</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
