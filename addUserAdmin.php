98c10cbb78a76a7f29da11e8374d101a6e53b0e4
Michal <mdrobena@rgu.ac.uk>
2016-11-30 15:36:29 +0000

    new

--- addUserAdmin.php
+++ addUserAdmin.php
@@ -22,7 +22,7 @@ if (mysqli_query($db, $sql)) {
     echo "New record created successfully";
 }
 else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
 }
 
 mysqli_close($db);
