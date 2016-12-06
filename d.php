24c83b77a955116211fad5e7d44fc075bb948819
Michal <mdrobena@rgu.ac.uk>
2016-12-06 15:34:38 +0000

    new

--- d.php
+++ d.php
@@ -1,5 +1,5 @@
 <?php
-define("JAVA_SERVLET", false); define("JAVA_HOSTS", 9267); require_once("Java.inc");
+require_once("Java.inc");
 
 echo new java("java.lang.String", "hello world");
 echo     java("java.lang.System")->getProperties();
