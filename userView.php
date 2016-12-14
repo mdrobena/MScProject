b3749d60926d3e0649fd95a56d1e495c3b7087a2
Michal <mdrobena@rgu.ac.uk>
2016-12-14 16:32:58 +0000

    new

--- userView.php
+++ userView.php
@@ -49,7 +49,7 @@
     </div>
 
 
-    <iframe src="https://idsdatanet.shinyapps.io/Test/" width="100%" height="100%"></iframe>
+    <iframe src="https://idsdatanet.shinyapps.io/Test/" width="100%" height="1000px"></iframe>
 
 
     <div class="footer">
