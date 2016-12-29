b02111b6af905deeb3a54f559fe37c4dbf554c2a
mdrobena <mdrobena@rgu.ac.uk>
2016-12-29 14:44:44 +0100

    g

--- login.php
+++ login.php
@@ -9,20 +9,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = mysqli_real_escape_string($db, $_POST['password']);
     $role = "";
 
-    $conn = $db;
+    $host = 'ap-cdbr-azure-east-c.cloudapp.net';
+    $db = 'md1511989';
+    $user = 'b21d7723d488a2';
+    $pass = '7359d184';
 
-    $stmt = $conn->prepare('SELECT role FROM people WHERE user_name = ? AND user_password = ?');
+    $mysqli = new mysqli($host, $user, $pass, $db);
+
+    $stmt = $mysqli->prepare('SELECT role FROM people WHERE user_name = ? AND user_password = ?');
     //$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $stmt->bindParam('ss', $username, $password);
+    $stmt->bindParam("ss", $username, $password);
     $stmt->execute();
     $stmt->bind_result($role);
     $stmt->store_result();
-    //$row = $stmt->fetch();
+    $row = $stmt->fetch();
 
 
     //$count = mysqli_num_rows($result);
     //$row = $stmt->fetch();
-    //$role = $row['role'];
+    $role = $row['role'];
     if($stmt->num_rows == 1) {
         if ($role == "user") {
             header("location: userView.php");
