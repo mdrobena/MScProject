e359c3f2f48f45187f191017ded00b15aa75c45a
mdrobena <mdrobena@rgu.ac.uk>
2016-12-29 14:29:42 +0100

    g

--- login.php
+++ login.php
@@ -3,29 +3,27 @@ session_start();
 
 include ("dbconnect.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
     $role = "";
 
     $host = 'ap-cdbr-azure-east-c.cloudapp.net';
-    $db   = 'md1511989';
+    $db = 'md1511989';
     $user = 'b21d7723d488a2';
     $pass = '7359d184';
     $charset = 'utf8';
-
+    /*
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
-    $pdo = new PDO($dsn, $user, $pass, $opt);
+    $pdo = new PDO($dsn, $user, $pass, $opt);*/
     $mysqli = new mysqli($host, $user, $pass, $db);
 
-
-    try {
 
     /*    $stmt = mysqli_stmt_init($db);
     //$username = mysqli_real_escape_string($db, $_POST['username']);
@@ -40,31 +38,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         mysqli_stmt_execute($stmt);
         mysqli_stmt_bind_result($stmt, $role);*/
 
-        $stmt = $mysqli->prepare('SELECT role FROM people WHERE user_name = ? AND user_password = ?');
-        //$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-        $stmt->bindParam('ss',$username, $password);
-        $stmt->execute();
-        $stmt->bind_result($role);
-        //$row = $stmt->fetch();
+    $stmt = $mysqli->prepare('SELECT role FROM people WHERE user_name = ? AND user_password = ?');
+    //$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $stmt->bindParam('ss', $username, $password);
+    $stmt->execute();
+    $stmt->bind_result($role);
+    $stmt->store_result();
+    //$row = $stmt->fetch();
 
 
     //$count = mysqli_num_rows($result);
     //$row = $stmt->fetch();
     //$role = $row['role'];
-
-    if ($role == "user") {
-        header("location: userView.php");
-        exit();
-    } elseif ($role == "admin") {
-        header("location: adminView.php");
-        exit();
-    } else {
-        header("location: index.html");
-        exit();
-    }
-
-    }catch(Exception $e){
-        echo $e->getMessage();
+    if($stmt->num_rows == 1) {
+        if ($role == "user") {
+            header("location: userView.php");
+            exit();
+        } elseif ($role == "admin") {
+            header("location: adminView.php");
+            exit();
+        } else {
+            header("location: index.html");
+            exit();
+        }
     }
 }
 else{echo 'dsds';}
